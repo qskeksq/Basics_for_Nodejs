042c6a49fb303252041e43913e385a67ca097750
qskeksq <qskeksq@gmail.com>
2018-01-11 02:38:40 +0900

    Update server.js

--- 3.basics/server.js
+++ 3.basics/server.js
@@ -1,45 +1,35 @@
 var http = require('http');
 var port = 3000;
 
-- 서버 생성
-    ```javaScript
-    var server = http.createServer(()=>{
-        console.log('server created');
-    });
-    ```
+// 1. 서버 생성
+var server = http.createServer(()=>{
+    console.log('server created');
+});
 
-- 서버 등록
-    ```javaScript
-    server.listen(3000, ()=>{
-        console.log('server is running');
-    });
-    ```
+// 2. 서버 등록
+server.listen(3000, ()=>{
+    console.log('server is running');
+});
 
-- 서버 연결
-    ```javaScript
-    server.on('connection', (socket)=>{
-        var addr = socket.address;
-        console.log('server connected : ['+addr.address+', '+addr.port+']');
-    });
-    ```
+// 3. 서버 연결
+server.on('connection', (socket)=>{
+    var addr = socket.address;
+    console.log('server connected : ['+addr.address+', '+addr.port+']');
+});
 
-- 클라이언트 요청 처리
-    ```javaScript
-    server.on('request', (request, response)=>{
-        console.log('server request on');
-        console.dir(request);
-        console.dir(response);
-        response.writeHead(200, {'Content-Type':'text/html; charset=utf-8'});
-        response.end();
-    });
-    ```
+// 4. 클라이언트 요청 처리
+server.on('request', (request, response)=>{
+    console.log('server request on');
+    console.dir(request);
+    console.dir(response);
+    response.writeHead(200, {'Content-Type':'text/html; charset=utf-8'});
+    response.end();
+});
 
-- 서버 종료
-    ```javaScript
-    server.on('close', ()=>{
-        console.log('server closed');
-    });
-    ```
+// 5. 서버 종료
+server.on('close', ()=>{
+    console.log('server closed');
+});
 
 
 
